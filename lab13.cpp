fe8824c0ac85e668581eee42eae60f2b255f0cb7
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2024-10-03 13:20:52 -0400

    Initial Commit

--- lab13.cpp
+++ lab13.cpp
@@ -1,13 +1,5 @@
 #include <cmath>
 
 double windChill(double t, double v){
-    double W;
-    if (t <= 50 and v > 3.0){
-        W = 35.74 + (0.6215 * t) - (35.75 * pow(v,0.16)) + (0.4275 * t * pow(v,0.16));
-    }
-    else{
-        W = t;   
-    }
-
-    return W;
+    
 }
